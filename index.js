762ced4b8c3ac14e07f514c5544c9c0d4b36f717
Ashley Rarieya <ararieya@gmail.com>
2023-09-25 08:11:40 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Write your solution in this file!
+const employee={
+    name: "Ashley Rarieya",
+    streetAddress: "Nairobi, Kenya"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {
+        ...employee,
+        [key]: value
+    };
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    employee[key] = value;
+    
+
+    return employee;
+  }
+
+  const updatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "Nairobi, Kenya");
+  
+  console.log(updatedEmployee); 
+
+  
+function deleteFromEmployeeByKey(employee, key) {
+  const clonedEmployee = { ...employee };
+
+  delete clonedEmployee[key];
+
+  return clonedEmployee;
+}
+
+const newEmployee = deleteFromEmployeeByKey(employee, "streetAddress");
+
+console.log(newEmployee); 
+console.log(employee);    
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    
+    return employee;
+  }
+  const modifiedEmployee = destructivelyDeleteFromEmployeeByKey(employee, "streetAddress");
+  
+  console.log(modifiedEmployee); 
+  console.log(employee);         
+  
+  
+  
+  
+  
+  
+  
+  
+  
